042c1d0789e143627246c8c212cbc4819c888677
Enrique Cametti <44720459+Quik-e@users.noreply.github.com>
2020-03-07 21:11:59 -0300

    Update agilent33220a_test.py

--- agilent33220a_test.py
+++ agilent33220a_test.py
@@ -3,16 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import arbitraryfunctions as af
 
-#instrument=agi.connect_device()
+instrument=agi.connect_device()
 #t=np.linspace(-32768,32767)
 #function=np.round(t/6553.6)
-function=af.jime()
+function=af.mati()
 plt.plot(function)
 plt.show()
 print(len(function))
-#instrument.load_signal(function,"MATI") #Solo acepta letras, numeros y '_' el nombre de la funcion
-#instrument.output_af("MATI",6,5000,99)
-#instrument.output_on()
-#instrument.catalog()
-#instrument.af_attributes("MATI")
+instrument.load_signal(function,"MATI") #Solo acepta letras, numeros y '_' el nombre de la funcion
+instrument.output_af("MATI",6,5000,99)
+instrument.output_on()
+instrument.catalog()
+instrument.af_attributes("MATI")
 #instrument.erase_af("QUIQUE_FUNC")
